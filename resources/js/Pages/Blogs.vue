f73413efb52124f61d98895272506e1b695ba986
Michaelkimani100 <michaelkimani854@gmail.com>
2021-04-16 07:04:10 +0300

    Jetsream Auth

---
+++ resources/js/Pages/Blogs.vue
@@ -0,0 +1,36 @@
+<template>
+    <app-layout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Blogs
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <button class="btn btn-primary">Add Blog</button>
+                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">                
+                   <div class="card py-3 px-5">
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+     
+    </app-layout>
+</template>
+
+<script>
+    import AppLayout from '@/Layouts/AppLayout'
+    export default {
+        components: {
+               AppLayout,
+             
+        },
+    }
+</script>
